strichmo/core-v-verif

integrate rvfi and rvvi work into cv32e40x
Signed-off-by: Steve Richmond <Steve.Richmond@silabs.com>

cv32e40x/env/uvme/uvme_cv32e40x_cntxt.sv
@@ -32,15 +32,14 @@ class uvme_cv32e40x_cntxt_c extends uvm_object;
    virtual uvmt_cv32e40x_debug_cov_assert_if debug_cov_vif;
 
    // Agent context handles
-   uvma_clknrst_cntxt_c    clknrst_cntxt;
-   uvma_interrupt_cntxt_c  interrupt_cntxt;
-   uvma_debug_cntxt_c      debug_cntxt;
-   uvma_obi_cntxt_c        obi_instr_cntxt;
-   uvma_obi_cntxt_c        obi_data_cntxt;
-   
-   // TODO Add scoreboard context handles
-   //      Ex: uvme_cv32e40x_sb_cntxt_c  sb_egress_cntxt;
-   //          uvme_cv32e40x_sb_cntxt_c  sb_ingress_cntxt;
+   uvma_clknrst_cntxt_c          clknrst_cntxt;
+   uvma_interrupt_cntxt_c        interrupt_cntxt;
+   uvma_debug_cntxt_c            debug_cntxt;
+   uvma_obi_cntxt_c              obi_instr_cntxt;
+   uvma_obi_cntxt_c              obi_data_cntxt;
+   uvma_rvfi_cntxt_c#(ILEN,XLEN) rvfi_cntxt;
+   uvma_rvvi_cntxt_c#(ILEN,XLEN) rvvi_cntxt;
+
    
    // Events
    uvm_event  sample_cfg_e;
@@ -53,6 +52,8 @@ class uvme_cv32e40x_cntxt_c extends uvm_object;
       `uvm_field_object(debug_cntxt  ,   UVM_DEFAULT)
       `uvm_field_object(obi_instr_cntxt, UVM_DEFAULT)
       `uvm_field_object(obi_data_cntxt,  UVM_DEFAULT)
+      `uvm_field_object(rvfi_cntxt,      UVM_DEFAULT)
+      `uvm_field_object(rvvi_cntxt,      UVM_DEFAULT)
       
       // TODO Add scoreboard context field macros
       //      Ex: `uvm_field_object(sb_egress_cntxt , UVM_DEFAULT)
@@ -77,16 +78,12 @@ function uvme_cv32e40x_cntxt_c::new(string name="uvme_cv32e40x_cntxt");
    
    clknrst_cntxt   = uvma_clknrst_cntxt_c::type_id::create("clknrst_cntxt");
    interrupt_cntxt = uvma_interrupt_cntxt_c::type_id::create("interrupt_cntxt");
-   //debug_cntxt = uvma_debug_cntxt_c::type_id::create("debug_cntxt");
    debug_cntxt = uvma_debug_cntxt_c::type_id::create("debug_cntxt");
    obi_instr_cntxt = uvma_obi_cntxt_c::type_id::create("obi_instr_cntxt");
    obi_data_cntxt  = uvma_obi_cntxt_c::type_id::create("obi_data_cntxt");
-   //debug_cntxt = uvma_debug_cntxt_c::type_id::create("debug_cntxt");
-   
-   // TODO Create uvme_cv32e40x_cntxt_c scoreboard context objects
-   //      Ex: sb_egress_cntxt  = uvma_cv32e40x_sb_cntxt_c::type_id::create("sb_egress_cntxt" );
-   //          sb_ingress_cntxt = uvma_cv32e40x_sb_cntxt_c::type_id::create("sb_ingress_cntxt");
-   
+   rvfi_cntxt = uvma_rvfi_cntxt_c#(ILEN,XLEN)::type_id::create("rvfi_cntxt");
+   rvvi_cntxt = uvma_rvvi_cntxt_c#(ILEN,XLEN)::type_id::create("rvvi_cntxt");
+
    sample_cfg_e   = new("sample_cfg_e"  );
    sample_cntxt_e = new("sample_cntxt_e");
    

vlsi-lab/cva6

cv32e40x/env/uvme/uvme_cv32e40x_cntxt.sv
@@ -32,15 +32,14 @@ class uvme_cv32e40x_cntxt_c extends uvm_object;
    virtual uvmt_cv32e40x_debug_cov_assert_if debug_cov_vif;
 
    // Agent context handles
-   uvma_clknrst_cntxt_c    clknrst_cntxt;
-   uvma_interrupt_cntxt_c  interrupt_cntxt;
-   uvma_debug_cntxt_c      debug_cntxt;
-   uvma_obi_cntxt_c        obi_instr_cntxt;
-   uvma_obi_cntxt_c        obi_data_cntxt;
-   
-   // TODO Add scoreboard context handles
-   //      Ex: uvme_cv32e40x_sb_cntxt_c  sb_egress_cntxt;
-   //          uvme_cv32e40x_sb_cntxt_c  sb_ingress_cntxt;
+   uvma_clknrst_cntxt_c          clknrst_cntxt;
+   uvma_interrupt_cntxt_c        interrupt_cntxt;
+   uvma_debug_cntxt_c            debug_cntxt;
+   uvma_obi_cntxt_c              obi_instr_cntxt;
+   uvma_obi_cntxt_c              obi_data_cntxt;
+   uvma_rvfi_cntxt_c#(ILEN,XLEN) rvfi_cntxt;
+   uvma_rvvi_cntxt_c#(ILEN,XLEN) rvvi_cntxt;
+
    
    // Events
    uvm_event  sample_cfg_e;
@@ -53,6 +52,8 @@ class uvme_cv32e40x_cntxt_c extends uvm_object;
       `uvm_field_object(debug_cntxt  ,   UVM_DEFAULT)
       `uvm_field_object(obi_instr_cntxt, UVM_DEFAULT)
       `uvm_field_object(obi_data_cntxt,  UVM_DEFAULT)
+      `uvm_field_object(rvfi_cntxt,      UVM_DEFAULT)
+      `uvm_field_object(rvvi_cntxt,      UVM_DEFAULT)
       
       // TODO Add scoreboard context field macros
       //      Ex: `uvm_field_object(sb_egress_cntxt , UVM_DEFAULT)
@@ -77,16 +78,12 @@ function uvme_cv32e40x_cntxt_c::new(string name="uvme_cv32e40x_cntxt");
    
    clknrst_cntxt   = uvma_clknrst_cntxt_c::type_id::create("clknrst_cntxt");
    interrupt_cntxt = uvma_interrupt_cntxt_c::type_id::create("interrupt_cntxt");
-   //debug_cntxt = uvma_debug_cntxt_c::type_id::create("debug_cntxt");
    debug_cntxt = uvma_debug_cntxt_c::type_id::create("debug_cntxt");
    obi_instr_cntxt = uvma_obi_cntxt_c::type_id::create("obi_instr_cntxt");
    obi_data_cntxt  = uvma_obi_cntxt_c::type_id::create("obi_data_cntxt");
-   //debug_cntxt = uvma_debug_cntxt_c::type_id::create("debug_cntxt");
-   
-   // TODO Create uvme_cv32e40x_cntxt_c scoreboard context objects
-   //      Ex: sb_egress_cntxt  = uvma_cv32e40x_sb_cntxt_c::type_id::create("sb_egress_cntxt" );
-   //          sb_ingress_cntxt = uvma_cv32e40x_sb_cntxt_c::type_id::create("sb_ingress_cntxt");
-   
+   rvfi_cntxt = uvma_rvfi_cntxt_c#(ILEN,XLEN)::type_id::create("rvfi_cntxt");
+   rvvi_cntxt = uvma_rvvi_ovpsim_cntxt_c#(ILEN,XLEN)::type_id::create("rvvi_cntxt");
+
    sample_cfg_e   = new("sample_cfg_e"  );
    sample_cntxt_e = new("sample_cntxt_e");
    