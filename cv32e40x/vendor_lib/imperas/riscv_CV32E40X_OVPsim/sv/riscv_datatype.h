f58d29be3c035802f581a6190a53c7ee41b8b120
eroom1966 <moore@imperas.com>
2021-06-14 16:53:51 +0100

    Trial test for shared structure between C/SV

--- cv32e40x/vendor_lib/imperas/riscv_CV32E40X_OVPsim/sv/riscv_datatype.h
+++ cv32e40x/vendor_lib/imperas/riscv_CV32E40X_OVPsim/sv/riscv_datatype.h
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright (c) 2005-2020 Imperas Software Ltd., www.imperas.com
+ * Copyright (c) 2005-2021 Imperas Software Ltd., www.imperas.com
  *
  * The contents of this file are provided under the Software License
  * Agreement that you accepted before downloading this file.
@@ -16,24 +16,28 @@
  *
  */
 
-`ifndef __INCL_TYPEDEFS_SV
-`define __INCL_TYPEDEFS_SV
-
-typedef byte     Int8;
-typedef shortint Int16;
-typedef int      Int32;
-typedef longint  Int64;
-typedef byte     unsigned Uns8;
-typedef shortint unsigned Uns16;
-typedef int      unsigned Uns32;
-typedef longint  unsigned Uns64;
-
-//
-// Address label monitor type
-//
 typedef struct {
-    int addr;
-    int enable;
-} watchT;
+    Uns64 retPC;
+    Uns64 excPC;
+    Uns64 nextPC;
 
-`endif
+    Uns64 x[32];
+    Uns64 f[32];
+    Uns64 csr[4096];
+
+    Uns64 count;
+    Uns64 trap;
+
+    Uns64 irq_ack_o;
+    Uns64 irq_id_o;
+    Uns64 DM;
+
+    Uns64 reset;
+    Uns64 deferint;
+    Uns64 irq_i;
+    Uns64 haltreq;
+    Uns64 resethaltreq;
+    Uns64 cycles;
+
+    Uns64 terminate;
+} sharedT;
