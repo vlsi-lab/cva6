e17443bf02e2c105b3dc0d06d9a930af7582bde8
alessandra.dolmeta <alessandra.dolmeta@polito.it>
2025-11-20 11:38:12 +0100

    cva6 working local

---
+++ tests/hello-world/hello_world.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Source environment setup
+source ./verif/sim/setup-env.sh
+
+DV_TARGET=cv64a6_imafdc_sv39
+
+# Set the NUM_JOBS variable to increase the number of parallel make jobs
+# export NUM_JOBS=
+
+export DV_SIMULATORS=veri-testharness
+# export DV_SIMULATORS=spike
+export TRACE_FAST=1
+
+cd ./verif/sim
+
+python3 cva6.py --target cv64a6_imafdc_sv39 --iss=$DV_SIMULATORS --iss_yaml=cva6.yaml \
+--c_tests ../../tests/hello-world/hello_world.c \
+--linker=../tests/custom/common/test.ld \
+--gcc_opts="-static -mcmodel=medany -fvisibility=hidden -O0 \
+-nostartfiles -g ../tests/custom/common/syscalls.c \
+../tests/custom/common/crt.S -lgcc \
+-I../tests/custom/env -I../tests/custom/common"
+
+cd ..
+cd ..
+
