49644d2d17b7fa61285b3e18692953fd46adf957
alessandra.dolmeta <alessandra.dolmeta@polito.it>
2026-02-06 14:35:30 +0100

    Hello-world

--- tests/ml-kem-512-indcpa/run.sh
+++ tests/ml-kem-512-indcpa/run.sh
@@ -18,26 +18,26 @@ cd ./verif/sim
 
 
 python3 cva6.py --target=$DV_TARGET --iss=$DV_SIMULATORS --iss_yaml=cva6.yaml \
-    --c_tests ../../tests/ml-kem-512/main.c \
+    --c_tests ../../tests/ml-kem-512-indcpa/main.c \
     --iss_timeout 1000000 --issrun_opts="+time_out=100000000000" \
     --linker=../tests/custom/common/test.ld \
     --gcc_opts="-static -mcmodel=medany -fvisibility=hidden -O2 \
     -funroll-loops -finline-functions \
     -nostartfiles -g ../tests/custom/common/syscalls.c \
     ../tests/custom/common/crt.S \
-    ../../tests/ml-kem-512/cbd.c \
-    ../../tests/ml-kem-512/fips202.c \
-    ../../tests/ml-kem-512/indcpa.c \
-    ../../tests/ml-kem-512/kem.c \
-    ../../tests/ml-kem-512/ntt.c \
-    ../../tests/ml-kem-512/poly.c \
-    ../../tests/ml-kem-512/polyvec.c \
-    ../../tests/ml-kem-512/randombytes.c \
-    ../../tests/ml-kem-512/reduce.c \
-    ../../tests/ml-kem-512/symmetric-shake.c \
-    ../../tests/ml-kem-512/verify.c \
+    ../../tests/ml-kem-512-indcpa/cbd.c \
+    ../../tests/ml-kem-512-indcpa/fips202.c \
+    ../../tests/ml-kem-512-indcpa/indcpa.c \
+    ../../tests/ml-kem-512-indcpa/kem.c \
+    ../../tests/ml-kem-512-indcpa/ntt.c \
+    ../../tests/ml-kem-512-indcpa/poly.c \
+    ../../tests/ml-kem-512-indcpa/polyvec.c \
+    ../../tests/ml-kem-512-indcpa/randombytes.c \
+    ../../tests/ml-kem-512-indcpa/reduce.c \
+    ../../tests/ml-kem-512-indcpa/symmetric-shake.c \
+    ../../tests/ml-kem-512-indcpa/verify.c \
     -lgcc -fstack-usage\
-    -I../tests/custom/env -I../tests/custom/common -I../../tests/ml-kem-512/inc"
+    -I../tests/custom/env -I../tests/custom/common -I../../tests/ml-kem-512-indcpa/inc"
 
 cd ..
 cd ..
